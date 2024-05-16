c54cd3a9416f0aea13a3228383cc185209ed14c8
Ruveydabal <ruveydabal024@gmail.com>
2024-05-16 12:01:29 +0200

    js
    js proberen

--- reis/html/pages/zoekopdracht.php
+++ reis/html/pages/zoekopdracht.php
@@ -7,9 +7,22 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
+<<<<<<< Updated upstream
 <?php
     include "../include/nav.php";
 ?>    
+=======
+
+    <!-- hier komt de navigatie -->
+    
+
+  <h1 id='titel'>hoi hoi</h1>
+
+    <button onclick='waarschuwing()'>klik hier</button>
+
+
+    <script src="../js/main.jsx"></script>
+>>>>>>> Stashed changes
 
 </body>
 </html>
