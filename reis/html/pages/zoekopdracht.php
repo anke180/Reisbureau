50a89b5794baacc85d883898abbb4d430c00a54d
Ruveydabal <ruveydabal024@gmail.com>
2024-05-27 12:54:34 +0200

    zoekopdracht
    verwijdert wat niet nodig is

--- reis/html/pages/zoekopdracht.php
+++ reis/html/pages/zoekopdracht.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&amp;display=swap" rel="stylesheet">
     <title>Document</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 
@@ -36,7 +36,7 @@
         <!-- informatie via database -->
         
         <?php
-             $stmt = $connection->query("SELECT * FROM verblijven  ");
+                $stmt = $connection->query("SELECT * FROM verblijven  ");
             while ($row = $stmt->fetch()){ 
 
                 echo "<div class='container-col'>";
@@ -85,39 +85,8 @@
                 echo "</div>";
          
 
-                                 }
-                            ?>
-                            <!-- hier komt de naam hotel + beooordeling hotel + informatie vakantieplek -->
-
-                        
-
-                        <!-- <div class="vakantie-blok-blauw">
-                            <div class="een-vlucht-informatie">
-
-                                <div class="balk-tekst-vlucht">
-
-                                    <a class="tekst-vlucht">heir komt een stuk tekst over de vlucht te staan</a>
-                                </div>
-
-                                <div class="balk-bekijk-vakantie">
-
-                                    <div class="bekijk-vakantie">
-
-                                        <a class="tekst-bekijk-vakantie">Bekijk vakantie</a>
-                                    </div>
-
-                                </div>
-                            </div>
-                                
-                        </div> -->
-
-
-
-                        
-                    
-
-           
-
+            }
+        ?>
         
     </div>
    
