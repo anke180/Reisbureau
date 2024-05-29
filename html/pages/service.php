6abc2af3a6e06294fe71393f562015e0f5e126f4
Renspeters <1195839@student.roc-nijmegen.nl>
2024-05-29 11:25:09 +0200

    Service Pagina

--- html/pages/service.php
+++ html/pages/service.php
@@ -10,13 +10,52 @@
     <div class="head">
         <div class="header">
             <div class="logo"></div>
-            <a href="" class="but1">Home</a>
+            <a href="../index.php" class="but1">Home</a>
             <a href="" class="but1">Bestemming</a>
-            <a href="" class="but1">Klantenservice</a>
-            <a href="" class="but1">Login</a>
+            <a href="service.php" class="but1">Klantenservice</a>
+            <a href="login.php" class="but1">Login</a>
         </div>
-        <div class="b1">
-            
+        <div class="b1" id="bckgrnd">
+        </div>
+        <div class="b3">
+            <div class="inf5">
+                <h2>Klachten, en nu?</h2>
+                <h3>Neem gerust contact op met een van onze collega’s die kunnen je verder helpen met uw problemen/vragen.</h3>
+            </div>
+            <div class="socmed">
+            <h2>Social media</h2>
+            <h3>Instagram</h3>
+            <h3>Facebook</h3>
+            <h3>TikTok</h3>
+            </div>
+            <div class="mailing">
+            <h2>E-mails verzenden naar onze experts:</h2>
+                <div class="p1" id="hgt">
+                    <h3>Naam: Ruveyda Bal</h3>
+                    <h3>E-mail: 1197794@student.roc-nijmegen.nl</h3></div>
+                <div class="p2" id="hgt">
+                    <h3>Naam: Anke van Dommelen</h3>
+                    <h3>E-mail: 1208834@student.roc-nijmegen.nl</h3>
+                </div>
+                <div class="p3" id="hgt">
+                    <h3>Naam: Rens Peters</h3>
+                    <h3>E-mail: 1195839@student.roc-nijmegen.nl</h3>
+                </div>
+            </div>
+            <div class="numb">
+            <h2>Telefoonnummers:</h2>
+                <div class="p1" id="hgt">
+                    <h3>Naam: Ruveyda Bal</h3>
+                    <h3>Nummer: 06 48795818</h3></div>
+                <div class="p2" id="hgt">
+                    <h3>Naam: Anke van Dommelen</h3>
+                    <h3>Nummer: 06 20065856</h3>
+                </div>
+                <div class="p3" id="hgt">
+                    <h3>Naam: Rens Peters</h3>
+                    <h3>Nummer: 06 30613747</h3>
+                </div>
+            </div>
         </div>
     </div>
 </body>
