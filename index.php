69f4f0bb90e167d313593932d57412aa69455403
anke180 <153827509+anke180@users.noreply.github.com>
2024-05-31 14:12:42 +0200

    bekijk verblijf

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
-    <div class="danger">
-        if($_SESSION['errormessage']){
+    ?><div class="danger">
+      <?php  if($_SESSION['errormessage']){
             echo $_SESSION['errormessage'];
         }
+        ?>
     </div>
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&amp;display=swap" rel="stylesheet">
         <title>Reisbureau home pagina</title>
-        <link rel="stylesheet" href="CSS/index.css">
+        <link rel="stylesheet" href="css/index.css">
     </head>
     <body>
         <?php
--- pages/bekijk_vakantie.php
+++ pages/bekijk_vakantie.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Reisbueau bekijk vakantie</title>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&amp;display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="../css/index.css">
     </head>
     <body>
         <header>
@@ -15,7 +16,7 @@
         </header>
         <main>
             <?php
-                $stmt = $connection->query("SELECT * FROM verblijven WHERE id=1 ");
+                $stmt = $connection->query("SELECT * FROM verblijven WHERE id=1");
                 while ($row = $stmt->fetch()){ 
                     echo "<div class='informatie-verblijf'>";
                         echo "<div class='naam-verblijf'>";
@@ -23,30 +24,30 @@
                             echo $row ['land-van-verblijf']."<br>\n";
                         echo "</div>"; //einde naam-verblijf
                         echo "<div class='plaatje-verblijf'>";
-                            echo "<img src=" . $row ['image'] . "><br>\n";
+                            echo "<img src=" . $row ['image'] . " width='750px' height='400px'><br>\n";
                         echo "</div>";//einde plaatje-verblijf
                         echo "<div class='informatie-en-prijs-verblijf'>";
                             echo "<div class='alle-informatie-verblijf'>";
-                                    echo ($row ['zwembad']) ? 'zwembad' : ''."<br>\n";
-                                    echo $row ['airco']."<br>\n";
-                                    echo $row ['wasmachine'] ? 'wasmachine': ''."<br>\n";
-                                    echo $row ['strand']."<br>\n";
-                                    echo $row ['all-inclusive']."<br>\n";
-                                    echo $row ['half-pension']."<br>\n";
-                                    echo $row ['vol-pension']."<br>\n";
-                                    echo $row ['glijbanen']."<br>\n";
-                                    echo $row ['animatie-kinderen']."<br>\n";
-                                    echo $row ['adult-only']."<br>\n";
-                                    echo $row ['spa']."<br>\n";
-                                    echo $row ['sport-plaats']."<br>\n";
-                                    echo $row ['bars']."<br>\n";
-                                    echo $row ['internet']."<br>\n";
-                                    echo $row ['restaurant']."<br>\n";
+                                echo ($row ['zwembad']) ? 'Zwembad' ."<br>\n" : '';
+                                echo ($row ['airco']) ? 'Airco' ."<br>\n" : '';
+                                echo ($row ['wasmachine']) ? 'Wasmachine' ."<br>\n" : '';
+                                echo ($row ['strand']) ? 'Strand' ."<br>\n" : '';
+                                echo ($row ['all-inclusive']) ? 'All-inclusive' ."<br>\n" : '';
+                                echo ($row ['half-pension']) ? 'Half-pension' ."<br>\n" : '';
+                                echo ($row ['vol-pension']) ? 'Vol-pension' ."<br>\n" : '';
+                                echo ($row ['glijbanen']) ? 'Glijbanen' ."<br>\n" : '';
+                                echo ($row ['animatie-kinderen']) ? 'Animatie-kinderen' ."<br>\n" : '';
+                                echo ($row ['adult-only']) ? 'Adult-only' ."<br>\n" : '';
+                                echo ($row ['spa']) ? 'Spa' ."<br>\n" : '';
+                                echo ($row ['sport-plaats']) ? 'Sport-plaats' ."<br>\n" : '';
+                                echo ($row ['bars']) ? 'Bars' ."<br>\n" : '';
+                                echo ($row ['internet'])? 'Internet' ."<br>\n" : '';
+                                echo ($row ['restaurant']) ? 'Restaurant' ."<br>\n" : '';
                             echo "</div>"; //alle-informatie-verblijf
                             echo "<div class='prijs-per-persoon-verblijf'>";
-                                echo "<a>€</a>";
+                                echo "<a class='tekst'>€</a>";
                                 echo $row ['prijs']."<br>\n";
-                                echo "<a>per persoon</a>";
+                                echo "<a class='tekst'>per persoon</a>";
                             echo "</div>";//prijs-per-persoon-verblijf
                         echo "</div>";//informatie-en-prijs-verblijf
                         echo "<div class='boeken-knop'>";
