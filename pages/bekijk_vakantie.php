6c3a3bffc46d9400e1e37dc730f899204dedcce8
anke180 <153827509+anke180@users.noreply.github.com>
2024-06-03 15:53:06 +0200

    bekijk vakantie

--- pages/bekijk_vakantie.php
+++ pages/bekijk_vakantie.php
@@ -16,7 +16,6 @@
         </header>
         <main>
             <?php
-            // RECENTIES MISSCHIEN NAAST HET PLAATJE VOOR MEER DINGEN OP DE PAGINA (WEL FAKE)
                 $_GET['id'];
                 $stmt = $connection->prepare("SELECT * FROM verblijven WHERE id = :id ");
                 $stmt->execute(['id' => $_GET['id']]);
@@ -27,19 +26,35 @@
                             echo $row ['naam']."<br>\n";
                             echo "</h1>";
                             echo $row ['land-van-verblijf']."<br>\n";
-                        echo "</div>"; //einde naam-verblijf
-                        echo "<div class='plaatje-verblijf'>";
-                            echo "<img src=" . $row ['image'] . " width='750px' height='400px'><br>\n";
-                        echo "</div>";//einde plaatje-verblijf
+                        echo "</div>"; 
+                        echo "<div class='plaatje-reacties-verblijf'>";
+                            echo "<div class='plaatje-verblijf'>";
+                                echo "<img src=" . $row ['image'] . " width='750px' height='400px'><br>\n";
+                            echo "</div>";
+                            echo "<div class='reacties-verblijf'>";
+                                echo "<div class='review1'>";
+                                    echo "<p>Review 1:</p>";
+                                    echo "<a class='review'>We hebben het fijn gehad. Mooie kamer. Ook was het personeel heel vriendelijk.  - Anoniem uit Nijmegen</a>";
+                                echo "</div>";
+                                echo "<div class='review2'>";
+                                    echo "<p>Review 2:</p>";
+                                    echo "<a class='review'>Niet wat we verwacht hadden!  - Anoniem uit van den Heuvel</a>";
+                                echo "</div>";
+                                echo "<div class='review3'>";
+                                    echo "<p>Review 3:</p>";
+                                    echo "<a class='review'>Ligging is echt geweldig, recht voor het strand en midden op de strip. Afkoelen in het zwembad ook prima.  - Anoniem uit Rotterdam</a>";
+                                echo "</div>";
+                            echo "</div>";
+                        echo "</div>";
                         echo "<div class='informatie-en-prijs-verblijf'>";
                             echo "<div class='alle-informatie-verblijf'>";
                                 echo ($row ['zwembad']) ? 'Zwembad' ."<br>\n" : '';
                                 echo ($row ['airco']) ? 'Airco' ."<br>\n" : '';
                                 echo ($row ['wasmachine']) ? 'Wasmachine' ."<br>\n" : '';
                                 echo ($row ['strand']) ? 'Strand' ."<br>\n" : '';
-                                echo ($row ['all-inclusive']) ? 'All-inclusive' ."<br>\n" : '';
-                                echo ($row ['half-pension']) ? 'Half-pension' ."<br>\n" : '';
-                                echo ($row ['vol-pension']) ? 'Vol-pension' ."<br>\n" : '';
+                                // echo ($row ['all-inclusive']) ? 'All-inclusive' ."<br>\n" : '';
+                                // echo ($row ['half-pension']) ? 'Half-pension' ."<br>\n" : '';
+                                // echo ($row ['vol-pension']) ? 'Vol-pension' ."<br>\n" : '';
                                 echo ($row ['glijbanen']) ? 'Glijbanen' ."<br>\n" : '';
                                 echo ($row ['animatie-kinderen']) ? 'Animatie-kinderen' ."<br>\n" : '';
                                 echo ($row ['adult-only']) ? 'Adult-only' ."<br>\n" : '';
@@ -48,17 +63,21 @@
                                 echo ($row ['bars']) ? 'Bars' ."<br>\n" : '';
                                 echo ($row ['internet'])? 'Internet' ."<br>\n" : '';
                                 echo ($row ['restaurant']) ? 'Restaurant' ."<br>\n" : '';
-                            echo "</div>"; //alle-informatie-verblijf
+                            echo "</div>"; 
                             echo "<div class='prijs-per-persoon-verblijf'>";
-                                echo "<a class='tekst'>€</a>";
-                                echo $row ['prijs']."<br>\n";
-                                echo "<a class='tekst'>per persoon</a>";
-                            echo "</div>";//prijs-per-persoon-verblijf
-                        echo "</div>";//informatie-en-prijs-verblijf
+                                echo '<div class="faciliteiten-lijst">';
+                                    echo "<a class='tekst'>De prijs per persoon is: €" . ($row ['prijs']) ."</a>";
+                                    echo ($row ['all-inclusive']) ? 'All-inclusive' : '';
+                                    echo ($row ['half-pension']) ? 'Half-pension' : '';
+                                    echo ($row ['vol-pension']) ? 'Vol-pension' : '';
+                                    echo "<a class='tekst'>De hoeveelheid slaapplekken in deze kamer is: " . ($row ['hoeveelheid-personen']) ."</a>";
+                                echo '</div>';
+                            echo "</div>";
+                        echo "</div>";
                         echo "<div class='boeken-knop'>";
                             echo "<a href='boeken.php' class='boeken-verblijf'>Boeken vakantie</a>";
-                        echo "</div>";//einde boeken-knop
-                    echo "</div>"; //einde informatie-bedrijf
+                        echo "</div>";
+                    echo "</div>"; 
                 }
             ?>
         </main>
