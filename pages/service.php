3051c97e6370b8f4f2a5ddd97f435761e0bcc3ae
Ruveydabal <ruveydabal024@gmail.com>
2024-06-12 12:06:26 +0200

    service pagina
    het "mail" versturen is klaar

--- pages/service.php
+++ pages/service.php
@@ -65,15 +65,35 @@
         </div>
 
         <div class="container-service-email">
+          <!-- <form action='service-verstuur-mail.php' name='service-verstuur-mail' method='POST'> -->
             <div class="blok-email">
                 <div class="mail-blok">
                     <div class="tekst-mail-sturen-balk">
                         <a class="tekst-mail-sturen">vul in om een mail te sturen</a>
                     </div>
-                    
+                    <div class="blok-lang-mail">
+                        <div class="blok-klein-mail">
+                            <div class="balk-groot-mail">
+                                <div class="balk-mail">
+                                <input class='mail'  type='text' name='email' placeholder='email'> 
+                                </div>
+                            </div>
+                            <div class="balk-groot-mail">
+                                <div class="blok-send">
+                                <input class='send' type='button' value='send'>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blok-klein-mail">
+                            <div class="blok-tekst-mail">
+                                <input class='tekst' type='text' name='tekst' placeholder='uw vraag.....'> 
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+      
 
 </body>
 </html>
