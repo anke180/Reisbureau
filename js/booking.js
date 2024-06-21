ee79f350664c9252a5fc684b0437f780665d8b02
Renspeters <1195839@student.roc-nijmegen.nl>
2024-06-21 14:22:00 +0200

    Booking update

--- js/booking.js
+++ js/booking.js
@@ -1,90 +1,98 @@
-<<<<<<< HEAD
 
 
-=======
- 
->>>>>>> origin/Ruveyda
 const setAmountOfGuests = () => {
+
     const amountOfGuests = document.querySelector('#amountGuests').value;
     const registerForm = document.querySelector('#registerform');
     console.log(amountOfGuests);
     for(let i = 0; i < amountOfGuests; i++) {
+
+        const voornaamLabel = document.createElement("label");
+        voornaamLabel.textContent = "Voornaam";
+
+        const persoonLabel = document.createElement("label");
+        persoonLabel.textContent = "Persoon " + i;
+
         const voornaamInput = document.createElement('input');
         voornaamInput.type = 'text';
-        voornaamInput.placeholder = 'Voornaam ' + i;
+        voornaamInput.placeholder = 'Voornaam ';
         voornaamInput.name = 'voornaam' + i;
-<<<<<<< HEAD
+        voornaamInput.classList.add('ggggg');
+
+        const adresLabel = document.createElement("label");
+        adresLabel.textContent = "Adres";
         
-=======
-       
->>>>>>> origin/Ruveyda
         const adresInput = document.createElement('input');
         adresInput.type = 'text';
-        adresInput.placeholder = 'Adres + huisnummer' + i;
+        adresInput.placeholder = 'Adres + huisnummer';
         adresInput.name = 'adres' + i;
-<<<<<<< HEAD
+        adresInput.classList.add('ggggg');
 
-=======
- 
->>>>>>> origin/Ruveyda
+        const achternaamLabel = document.createElement("label");
+        achternaamLabel.textContent = "Achternaam";
+
         const achternaamInput = document.createElement('input');
         achternaamInput.type = 'text';
-        achternaamInput.placeholder = 'Achternaam ' + i;
+        achternaamInput.placeholder = 'Achternaam ';
         achternaamInput.name = 'achternaam' + i;
-<<<<<<< HEAD
+        achternaamInput.classList.add('ggggg');
 
-=======
- 
->>>>>>> origin/Ruveyda
+        const emailLabel = document.createElement("label");
+        emailLabel.textContent = "email";
+
         const emailInput = document.createElement('input');
         emailInput.type = 'email';
-        emailInput.placeholder = 'Email ' + i;
+        emailInput.placeholder = 'Email ';
         emailInput.name = 'email' + i;
-<<<<<<< HEAD
+        emailInput.classList.add('ggggg');
+
+        const geboortedatumLabel = document.createElement("label");
+        geboortedatumLabel.textContent = "Geboortedatum";
 
         const geboortedatumInput = document.createElement('input');
         geboortedatumInput.type = 'date';
-        geboortedatumInput.placeholder = 'Geboortedatum ' + i;
+        geboortedatumInput.placeholder = 'Geboortedatum ';
         geboortedatumInput.name = 'geboortedatum' + i; 
+        geboortedatumInput.classList.add('ggggg');
+
+        const geslachtLabel = document.createElement("label");
+        geslachtLabel.textContent = "Geslacht";
 
         const geslachtInput = document.createElement('input');
         geslachtInput.type = 'option';
-        geslachtInput.placeholder = 'geslacht ' + i;
+        geslachtInput.placeholder = 'geslacht ';
         geslachtInput.name = 'geslacht' + i; 
+        geslachtInput.classList.add('ggggg');
+
+        const bankgegevensLabel = document.createElement("label");
+        bankgegevensLabel.textContent = "Bankgegevens";
 
         const bankgegevensInput = document.createElement('input');
         bankgegevensInput.type = 'text';
-        bankgegevensInput.placeholder = 'banknummer ' + i;
+        bankgegevensInput.placeholder = 'banknummer ';
         bankgegevensInput.name = 'bankgegevens' + i; 
+        bankgegevensInput.classList.add('ggggg');
         
-        
-        
-=======
- 
-        const geboortedatumInput = document.createElement('input');
-        geboortedatumInput.type = 'date';
-        geboortedatumInput.placeholder = 'Geboortedatum ' + i;
-        geboortedatumInput.name = 'geboortedatum' + i;
- 
-        const geslachtInput = document.createElement('input');
-        geslachtInput.type = 'option';
-        geslachtInput.placeholder = 'geslacht ' + i;
-        geslachtInput.name = 'geslacht' + i;
- 
-        const bankgegevensInput = document.createElement('input');
-        bankgegevensInput.type = 'text';
-        bankgegevensInput.placeholder = 'banknummer ' + i;
-        bankgegevensInput.name = 'bankgegevens' + i;
-       
-       
-       
->>>>>>> origin/Ruveyda
-        registerForm.appendChild( voornaamInput);
-        registerForm.appendChild( achternaamInput);
-        registerForm.appendChild( adresInput);
-        registerForm.appendChild( emailInput);
-        registerForm.appendChild( geboortedatumInput);
-        registerForm.appendChild( geslachtInput);
-        registerForm.appendChild( bankgegevensInput);
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('form-container');
+
+
+        formContainer.appendChild(persoonLabel);
+        formContainer.appendChild(voornaamLabel);
+        formContainer.appendChild( voornaamInput);
+        formContainer.appendChild(achternaamLabel);
+        formContainer.appendChild( achternaamInput);
+        formContainer.appendChild(geboortedatumLabel);
+        formContainer.appendChild( geboortedatumInput);
+        formContainer.appendChild(adresLabel);
+        formContainer.appendChild( adresInput);
+        formContainer.appendChild(emailLabel);
+        formContainer.appendChild( emailInput);
+        formContainer.appendChild(geslachtLabel);
+        formContainer.appendChild( geslachtInput);
+        formContainer.appendChild(bankgegevensLabel);
+        formContainer.appendChild( bankgegevensInput);
+
+        registerForm.appendChild(formContainer);
     }
 }
--- pages/booking.php
+++ pages/booking.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../CSS/index.css">
     <script src="../js/booking.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title>Reisbureau</title>
@@ -22,14 +22,12 @@
         <label>Hoeveel gasten?</label>
         <input  type="number" id='amountGuests' name="aantal_gasten" placeholder="1" class="but4" >
         <a onclick="setAmountOfGuests();" class="but3">Reizigers toevoegen</a>
- 
+
         <button type="submit" name="submit" value="submit" class="but3">Boeken</button>
+        
     </form>
 </div>
 <br>
 </div>
-    <!-- <?php
-        include "../include/footer.php"
-    ?> -->
 </body>
 </html>
